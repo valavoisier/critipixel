afc98e30f2c9079b7d5913e8551211b652285e76
Valerie Lavoisier <val.lavoisier3@gmail.com>
2026-04-29 14:02:58 +0200

    feat: création tests/Functional/Review/AddReviewTest - squelette de la classe avec les 3 méthodes de test

---
+++ tests/Functional/Review/AddReviewTest.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Functional\Review;
+
+use App\Tests\Functional\FunctionalTestCase;
+
+final class AddReviewTest extends FunctionalTestCase
+{
+    public function testAddReviewNominal(): void
+    {
+        // TODO
+    }
+
+    public function testAddReviewValidationErrors(): void
+    {
+        // TODO
+    }
+
+    public function testAddReviewUnauthorizedUser(): void
+    {
+        // TODO
+    }
+}
