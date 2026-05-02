960b0206636ab75540bcf32efe7b1c5b8d568f7f
Valerie Lavoisier <val.lavoisier3@gmail.com>
2026-05-02 13:44:33 +0200

    fix(form): ajoute les generics PHPStan sur les Form Types
    - FilterType : @extends AbstractType<Filter>
    - RegisterType : @extends AbstractType<User>
    - ReviewType : @extends AbstractType<Review>

--- src/Form/RegisterType.php
+++ src/Form/RegisterType.php
@@ -12,6 +12,9 @@ use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
+/**
+ * @extends AbstractType<User>
+ */
 final class RegisterType extends AbstractType
 {
     public function configureOptions(OptionsResolver $resolver): void
--- src/Form/ReviewType.php
+++ src/Form/ReviewType.php
@@ -12,6 +12,9 @@ use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
+/**
+ * @extends AbstractType<Review>
+ */
 final class ReviewType extends AbstractType
 {
     public function configureOptions(OptionsResolver $resolver): void
